2b0553368b5cdebdd7fb68d54afe128b572d609d
kadu66 <kadu.rodrigo.1bmangal@gmail.com>
2025-09-08 08:15:05 -0300

    Add files via upload

---
+++ 5.js
@@ -0,0 +1,10 @@
+// 5) DO...WHILE
+
+// Parecido com o while, mas garante que executa pelo menos uma vez
+let numero = 1;
+
+console.log("Contagem com do...while:");
+do {
+ console.log(numero); // mostra o valor
+ numero++; // incrementa, ou seja, 2, até chegar em 5, linha seguinte
+} while (numero <= 5); // verifica a condição
